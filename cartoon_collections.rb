32bf8b2257c03e39cefc59c3d5a7dab170fe5ea3
Eden Mesfun <emesfun@gmail.com>
2018-01-25 18:32:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(name)
+  name.each_with_index do |name, index|
+    puts "#{index+1}.#{name}"
+  end #do iteration
+end #method
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |call| call.capitalize + "!"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(long_calls)
+  if long_calls.length >= 4
+    true
+  else
+    false
+  end #if statement
+end #method
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def find_the_cheese(cheese_find)
+  nil unless cheese_find.include?("cheddar" || "gouda" || "camembert")
+  if cheese_find.include?("cheddar")
+    return "cheddar"
+  elsif cheese_find.include?("gouda")
+    return "gouda"
+  elsif cheese_find.include?("camembert")
+    return "camembert"
+  end #if statement
+end #method
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
